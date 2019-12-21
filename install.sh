1d88b8c7146055e519eb68051db791d78be300bd
James Hong <ojameso@hotmail.com>
2019-12-20 23:15:24 -0800

    Update for tsx, etc

--- install.sh
+++ install.sh
@@ -2,8 +2,17 @@
 #homebrew
 #rbenv
 
+#bash4
+#bashcompletion@2
+
+#silversearch requires brew installation
+#neovim requires python3
+
 #postgres
+#mysql
 #pgcli for psql syntax highlighting
 
 #symlinks for vimrc bashrc
 #git completion
+
+#ssh
