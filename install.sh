8dd135bf103f0627419cae771c3352458b9da853
James Hong <ojameso@hotmail.com>
2020-11-18 00:18:13 -0800

    Add details to install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,25 @@
 #brew install rbenv
 #brew install neovim
 #neovim requires python3
+#neovim requires config
+#.config/nvim/init.vim (add source ~/.vimrc here)
+#symlink colors in nvim config dir if can't find
+
+#when doing gem install rails and getting nokogiri issues
+#try `brew unlink gcc`
+#symlink files
+#ln -s ~/.vimrc ~/dotfiles/vim/vimrc
+
+#create .vim dir as well as...
+# .vim/backup
+# .vim/swap
+# .vim/undo
 
 #add vundle
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #:PluginInstall in vim
+#:UpdateRemotePlugins in vim
 #need to run install.sh in bundle/nvim-javascript/
-#silversearch requires brew installation
 
 #bash4
 #bashcompletion@2
